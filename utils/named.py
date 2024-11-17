be51d24449a62f47e5360ac26cda9080f66ca204
Grigor Farishyan <farishyan@gmail.com>
2024-11-17 12:52:14 +0400

    add named confguratn setup

--- utils/named.py
+++ utils/named.py
@@ -13,3 +13,18 @@ def install_named():
   subprocess.run(["systemctl start named"], check=True)
   subprocess.run(["systemctl enable named"], check=True)
   
+  
+def configure_named():
+  targetDir = "/var/named/chroot"
+  srcDir="/etc/bind"
+  
+  uid=subprocess.run(["id -u named"])
+  gid=subprocess.run(["id -g named"])
+  
+  os.makedirs(targetDir, exist_ok=True)
+  shutil.copytree(srcDir, targetDir)
+  shutil.chown(targetDir, uid, gid)
+  subprocess.call(["chmod", "-R", "755", targetDir])
+  subprocess.call(["systemctl", "start", "named-chroot"])
+  subprocess.call(["systemctl", "enable", "named-chroot"])
+  
