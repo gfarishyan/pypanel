2ad29adc87f56873633230cb70ca91f351c44f71
Grigor Farishyan <farishyan@gmail.com>
2024-11-17 23:01:05 +0400

    fixed named scipt

--- utils/named.py
+++ utils/named.py
@@ -4,32 +4,14 @@ import subprocess
 
 
 def install_named():
-  status = subprocess.run(["dnf", "install", "bind", "bind-chroot", "bind-libs", "bind-utils", "-y"], text = True, check=False)
-  
-  if status != 0:
-    print("Error occured.")
-    return
-  
-  
-  subprocess.run(["setsebool -P named_write_master_zones on"], check=True)
-  subprocess.run(["setsebool -P named_enable_zone_write on"], check=True)
-  subprocess.run(["semanage fcontext -a -t named_zone_t \"/var/named(/.*)?\""], check=True)
-  subprocess.run(["semanage fcontext -a -t named_conf_t \"/etc/named.conf\""], check=True)
-  subprocess.run(["restorecon -Rv \"/var/named\" \"/etc/named.conf\" \"/var/named/data\""], check=True)
-  subprocess.run(["systemctl start named"], check=True)
-  subprocess.run(["systemctl enable named"], check=True)
+  subprocess.run(["dnf", "install", "bind", "bind-chroot", "bind-libs", "bind-utils", "-y"], text = True, check=False)
+  #subprocess.run(["setsebool -P named_write_master_zones on"], check=True)
+  #subprocess.run(["setsebool -P named_enable_zone_write on"], check=True)
+  #subprocess.run(["semanage fcontext -a -t named_zone_t \"/var/named(/.*)?\""], check=True)
+  #subprocess.run(["semanage fcontext -a -t named_conf_t \"/etc/named.conf\""], check=True)
+  #subprocess.run(["restorecon -Rv \"/var/named\" \"/etc/named.conf\" \"/var/named/data\""], check=True)
+  subprocess.run(["systemctl", "start", "named"], text = True, check=False)
+  subprocess.run(["systemctl", "enable", "named"], text = True, check=False)
   
 def configure_named():
-  targetDir = "/var/named/chroot"
-  srcDir="/etc/bind"
-  
-  uid=subprocess.run(["id -u named"])
-  gid=subprocess.run(["id -g named"])
-  
-  os.makedirs(targetDir, exist_ok=True)
-  shutil.copytree(srcDir, targetDir)
-  shutil.chown(targetDir, uid, gid)
-  subprocess.call(["chmod", "-R", "755", targetDir])
-  subprocess.call(["systemctl", "start", "named-chroot"])
-  subprocess.call(["systemctl", "enable", "named-chroot"])
-  
+  return
